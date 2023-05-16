48aca74203e66dd8c9018ae86cd2e982b3888011
Diego Vargas <diegovs0504@gmail.com>
2023-05-16 13:26:55 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type IAppProps = {
+  //
+}
+
+const App: React.FC<IAppProps> = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export { App };
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '300px',
+      's': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '3000px'
+    }
+  },
+  plugins: [],
+}
+
