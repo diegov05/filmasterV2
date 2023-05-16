abf8aa636717b63a7b42f2d127a434100a8da06a
Diego Vargas <diegovs0504@gmail.com>
2023-05-16 17:44:00 -0400

    Created Loading Component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./Loading.css"
+
+interface LoadingProps {
+
+}
+
+
+const Loading: React.FC<LoadingProps> = () => {
+    const size = 40;
+    const strokeWidth = 5;
+    const radius = (size - strokeWidth) / 2;
+    const circumference = 2 * Math.PI * radius;
+
+    console.log(size, strokeWidth, radius, circumference)
+
+    return (
+        <svg
+            viewBox={`0 0 ${size} ${size}`}
+            width={size}
+            height={size}
+            className="animate-spin"
+        >
+            <circle
+                cx={size / 2}
+                cy={size / 2}
+                r={17.5}
+                fill="transparent"
+                stroke="#000"
+                strokeWidth={5}
+                strokeDasharray={190}
+                strokeDashoffset={110}
+            />
+        </svg>
+    );
+};
+
+export default Loading;
