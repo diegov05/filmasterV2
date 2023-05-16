3cd71b04da375ec4017452bf471ac87ba2e62a19
Diego Vargas <diegovs0504@gmail.com>
2023-05-16 17:41:48 -0400

    Imported All Brand Logos

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import logoBlack from "./logo/logo_black.png"
+import logoWhite from "./logo/logo.png"
+import logoIcon from "./logo/logo_img.png"
+import logoPurpleIcon from "./logo/logo_img_purple.png"
+import topography from './topography.svg'
+import gradient from './purple-golden-gradient.png'
+
+export default {
+    logoBlack,
+    logoWhite,
+    logoIcon,
+    logoPurpleIcon,
+    topography,
+    gradient
+}
