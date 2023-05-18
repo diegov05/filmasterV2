2496c732a924bb7d6b5579517337d650e3049b3d
Diego Vargas <diegovs0504@gmail.com>
2023-05-18 15:22:58 -0400

    Update AddToWatchList.tsx

--- src/components/AddToWatchList/AddToWatchList.tsx
+++ src/components/AddToWatchList/AddToWatchList.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import { BookmarkIcon } from '@heroicons/react/24/outline'
 
 interface AddToWatchListProps {
 
@@ -7,7 +8,10 @@ interface AddToWatchListProps {
 const AddToWatchList: FC<AddToWatchListProps> = () => {
     return (
         <div>
-            AddToWatchList
+            <button className='flex flex-row gap-2 justify-center items-center px-5 py-3 bg-bg-color text-text-color rounded-2xl font-bold text-xs sm:max-4xl:text-lg transition-all hover:bg-text-color hover:text-bg-color'>
+                Add to Watch List
+                <BookmarkIcon className='sm:max-4xl:w-6 sm:max-4xl:h-6 w-4 h-4' />
+            </button>
         </div>
     )
 }
