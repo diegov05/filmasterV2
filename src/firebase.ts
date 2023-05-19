f19a0526982f69b69408d01f26982ffa7064a685
Diego Vargas <diegovs0504@gmail.com>
2023-05-19 13:00:28 -0400

    Added Firestore Database

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { config } from './config/config';
+import { getFirestore } from 'firebase/firestore'
 
 const app = initializeApp(config.firebaseConfig);
 const auth = getAuth(app);
 
-export { auth };
+const db = getFirestore()
+
+export { auth, db };
