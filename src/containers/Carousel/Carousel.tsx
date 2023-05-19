1bc4dd590eefffb4d0cd0382b43373389118c9e6
Diego Vargas <diegovs0504@gmail.com>
2023-05-19 11:13:58 -0400

    Implemented Carousel Arrows Functionality

--- src/containers/Carousel/Carousel.tsx
+++ src/containers/Carousel/Carousel.tsx
@@ -1,60 +1,77 @@
-import { FC, useEffect, useRef, useState } from 'react'
-import { Movie } from '../../interfaces/interfaces'
-import { requests } from '../../requests'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
-import { Loading, MoviePoster } from '../../components'
-import images from "../../assets"
-import "./Carousel.css"
-import axios from 'axios'
+import { FC, useEffect, useRef, useState } from 'react';
+import { Movie } from '../../interfaces/interfaces';
+import { requests } from '../../requests';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import { Loading, MoviePoster } from '../../components';
+import images from "../../assets";
+import "./Carousel.css";
+import axios from 'axios';
 
 interface CarouselProps {
-    content: string
+    content: string;
 }
 
 const Carousel: FC<CarouselProps> = (props) => {
-
-    const [shows, setShows] = useState<Movie[]>([])
-    const [showLeftArrow, setShowLeftArrow] = useState<boolean>(false);
-    const content = props.content
+    const [shows, setShows] = useState<Movie[]>([]);
+    const [showLeftButton, setShowLeftButton] = useState<boolean>(false);
+    const content = props.content;
 
     const containerRef = useRef<HTMLDivElement>(null);
 
+    const handleScroll = () => {
+        const container = containerRef.current;
+        if (container) {
+            setShowLeftButton(container.scrollLeft > 0);
+        }
+    };
+
     const handleLeftClick = () => {
         containerRef.current?.scrollBy({
-            left: -400,
+            left: -300,
             behavior: "smooth",
         });
     };
 
     const handleRightClick = () => {
         containerRef.current?.scrollBy({
-            left: 400,
+            left: 300,
             behavior: "smooth",
         });
-    }
+    };
 
     useEffect(() => {
         switch (content) {
             case "movies":
                 axios.get(requests.popularMoviesRequest).then((response) => {
                     setShows(response.data.results);
-                })
-
+                });
                 break;
             case "series":
                 axios.get(requests.popularSeriesRequest).then((response) => {
                     setShows(response.data.results);
-                })
+                });
                 break;
         }
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container) {
+            container.addEventListener("scroll", handleScroll);
+        }
+        return () => {
+            if (container) {
+                container.removeEventListener("scroll", handleScroll);
+            }
+        };
+    }, []);
 
     if (!shows) {
         return (
             <div className='w-full h-screen flex flex-col justify-center items-center'>
                 <Loading />
             </div>
-        )
+        );
     }
 
     return (
@@ -66,48 +83,59 @@ const Carousel: FC<CarouselProps> = (props) => {
                         <h1 className='w-max font-bold text-2xl xs:max-sm:text-3xl sm:max-4xl:text-4xl pb-4 text-text-color'>Movies</h1>
                         <div className='w-full h-1 bg-gradient' />
                     </div>
-                    <div className="relative flex items-center justify-start overflow-x-auto">
-                        <div className="flex flex-col items-start">
-                            <div ref={containerRef} className="carousel flex overflow-x-visible gap-4 sm:max-4xl:gap-12 pb-10">
-                                {shows.map((show) => (
-                                    <MoviePoster key={show.id} movie={show} />
-                                ))}
+                    <div className='flex flex-row justify-between items-center'>
+                        {showLeftButton && (
+                            <button className="hidden lg:max-4xl:block px-2 py-1" onClick={handleLeftClick}>
+                                <ChevronLeftIcon className="hidden sm:max-4xl:block w-8 h-8 text-text-color" />
+                            </button>
+                        )}
+                        <div ref={containerRef} className="no-scrollbar relative flex flex-row items-center justify-between overflow-x-auto">
+                            <div className="flex flex-col items-start">
+                                <div className="flex overflow-x-visible gap-4 sm:max-4xl:gap-12 pb-10">
+                                    {shows.map((show) => (
+                                        <MoviePoster key={show.id} movie={show} />
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                        <button
-                            className="px-2 py-1"
-                            onClick={handleRightClick}
-                        >
-                            <ChevronRightIcon className="hidden sm:max-4xl:block w-8 h-8" />
+                        <button className="hidden lg:max-4xl:block px-2 py-1" onClick={handleRightClick}>
+                            <ChevronRightIcon className="hidden sm:max-4xl:block w-8 h-8 text-text-color" />
                         </button>
                     </div>
                 </div>
             )}
             {content === "series" && (
-                <div className='flex flex-col gap-4 s:max-md:gap-8 md:max-4xl:gap-16'>
+                <div className='slide-in-fwd-center flex flex-col gap-4 s:max-md:gap-8 md:max-4xl:gap-16'>
                     <div className='w-max'>
                         <h1 className='w-max font-bold text-2xl xs:max-sm:text-3xl sm:max-4xl:text-4xl pb-4 text-text-color'>Series</h1>
                         <div className='w-full h-1 bg-gradient' />
                     </div>
-                    <div className="relative flex items-center justify-start overflow-x-auto">
-                        <div className="flex items-start">
-                            <div ref={containerRef} className="carousel flex overflow-x-visible gap-4 sm:max-4xl:gap-12 pb-10 scroll-snap-x-mandatory snap-center">
-                                {shows.map((show) => (
-                                    <MoviePoster key={show.id} movie={show} />
-                                ))}
+                    <div className='flex flex-row justify-between items-center'>
+                        {showLeftButton && (
+                            <button className="hidden lg:max-4xl:block px-2 py-1" onClick={handleLeftClick}>
+                                <ChevronLeftIcon className="hidden sm:max-4xl:block w-8 h-8 text-text-color" />
+                            </button>
+                        )}
+                        <div ref={containerRef} className="no-scrollbar relative flex flex-row items-center justify-between overflow-x-auto">
+                            <div className="flex flex-col items-start">
+                                <div className="flex overflow-x-visible gap-4 sm:max-4xl:gap-12 pb-10">
+                                    {shows.map((show) => (
+                                        <MoviePoster key={show.id} movie={show} />
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                        <button
-                            className="px-2 py-1"
-                            onClick={handleRightClick}
-                        >
-                            <ChevronRightIcon className="hidden sm:max-4xl:block w-8 h-8" />
+                        <button className="hidden lg:max-4xl:block px-2 py-1" onClick={handleRightClick}>
+                            <ChevronRightIcon className="hidden sm:max-4xl:block w-8 h-8 text-text-color" />
                         </button>
                     </div>
                 </div>
             )}
+            {content === "whatToWatch" && (
+                <div></div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export { Carousel };
