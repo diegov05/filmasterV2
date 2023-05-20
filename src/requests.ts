c2803dfb4d05dcfc3ec6fdddd41590ea63b89709
Diego Vargas <diegovs0504@gmail.com>
2023-05-20 15:23:51 -0400

    Removed unused API Endpoints

--- src/requests.ts
+++ src/requests.ts
@@ -2,9 +2,6 @@ const key: string = "bf0048d3c2f01f2c4e53d30b41cea183"
 
 const requests = {
     popularMoviesRequest: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&page=1&without_genres=27,14`,
-    latestMoviesRequest: `
-    https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US&without_genres=27,14`,
-    topRatedMoviesRequest: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1&without_genres=27,14`,
     upcomingMoviesRequest: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1&without_genres=27,14`,
     trendingMoviesRequest: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}&without_genres=27,14`,
     nowPlayingMovies: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
