5b5110bcc9fefc8df5ff990e73a8592cf437ad18
Diego Vargas <diegovs0504@gmail.com>
2023-05-19 20:58:02 -0400

    Exported New Containers

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,10 +2,20 @@ import { Carousel } from "./Carousel/Carousel";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { MovieHeader } from "./MovieHeader/MovieHeader";
+import { Trailer } from "./Trailer/Trailer";
+import { Overview } from "./Overview/Overview";
+import { MovieDetails } from "./MovieDetails/MovieDetails";
+import { Reviews } from "./Reviews/Reviews";
+import { Cast } from "./Cast/Cast";
 
 export {
     Carousel,
     Footer,
     Header,
-    MovieHeader
+    MovieHeader,
+    Trailer,
+    MovieDetails,
+    Overview,
+    Reviews,
+    Cast
 }
