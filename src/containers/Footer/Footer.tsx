5fad551cccf61021e6437deb87f4b0e628bc0c4a
Diego Vargas <diegovs0504@gmail.com>
2023-05-22 13:48:50 -0400

    Update Footer.tsx

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -15,14 +15,14 @@ const Footer: FC<FooterProps> = () => {
                     Cinematic Excellence<br />
                     Filmaster
                 </h1>
-                <a href="https://github.com/diegov05/filmasterV2" target='__blank'>
+                <a className='w-min' href="https://github.com/diegov05/filmasterV2" target='__blank'>
                     <button className='flex flex-row justify-start items-center gap-2 bg-gradient cursor-pointer transition-all duration-300 hover:opacity-80 py-2 px-8 md:max-4xl:py-4 w-max md:max-4xl:px-16 rounded-2xl font-bold text-sm md:max-4xl:text-xl text-bg-color'>
                         Learn More
                         <ArrowTopRightOnSquareIcon className='w-6 h-6' />
                     </button>
                 </a>
-                <a href="https://github.com/diegov05" target='__blank'>
-                    <button className='flex flex-row justify-start items-center gap-2.5 bg-[#0d1017] cursor-pointer transition-all duration-300 hover:bg-bg-color hover:text-text-color py-2 px-8 md:max-4xl:py-4 w-max md:max-4xl:px-16 rounded-2xl font-bold text-sm md:max-4xl:text-xl text-bg-color'>
+                <a className='w-min' href="https://github.com/diegov05" target='__blank'>
+                    <button className='flex flex-row justify-start items-center gap-2.5 bg-[#0d1017] cursor-pointer transition-all duration-300 hover:shadow-xl hover:shadow-zinc-300 py-2 px-8 md:max-4xl:py-4 w-max md:max-4xl:px-16 rounded-2xl font-bold text-sm md:max-4xl:text-xl text-bg-color'>
                         diegov05
                         <AiFillGithub className='w-6 h-6' />
                     </button>
